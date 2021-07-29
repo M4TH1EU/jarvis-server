bcc44f9fbd30eb7abc6413edc63ff2e017ee2f09
Mathieu B <m4th1eu@protonmail.com>
2021-07-29 23:29:53 +0200

    Disabled all tests recognise in main.py

--- jarvis/main.py
+++ jarvis/main.py
@@ -47,9 +47,10 @@ if __name__ == '__main__':
 
     intent_manager.process_handlers()
 
-    # intent_manager.recognise("cherche sur wikipedia Elon Musk")
-    # intent_manager.recognise("raconte moi une blague")
-    # intent_manager.recognise("joue le morceau crazy crazy nights de KISS sur spotify")
+    # intent_manager.recognise("cherche sur wikipedia Elon Musk")  # TO CHECK
+    # intent_manager.recognise("raconte moi une blague")  # WORKING
+    # intent_manager.recognise("joue le morceau crazy crazy nights de KISS sur spotify")  # WORKING
+    # intent_manager.recognise("coupe la musique")  # WORKING
 
     # start the flask server
     app.config['JSON_AS_ASCII'] = False
