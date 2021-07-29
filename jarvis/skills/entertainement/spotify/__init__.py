42a3323a6b3222696835198e634d279cdbf0b82a
Mathieu B <m4th1eu@protonmail.com>
2021-07-29 23:29:42 +0200

    Pause music handler and finished play_a_song handler, both work fine.

--- jarvis/skills/entertainement/spotify/__init__.py
+++ jarvis/skills/entertainement/spotify/__init__.py
@@ -15,5 +15,15 @@ class SpotifySkill(Skill, metaclass=SkillRegistering):
                 song_lists_matching = spotify.query_song(data['song'], data['singer'])
 
             if song_lists_matching is not None and len(song_lists_matching) >= 1:
-                print(song_lists_matching[0]['uri'] + " / " + song_lists_matching[0]['name'] + " / " +
-                      song_lists_matching[0]['artists'][0]['name'])
+                print(
+                    "[INFO INTENT] - Now playing : " + song_lists_matching[0]['uri'] + " / " + song_lists_matching[0][
+                        'name'] + " / " +
+                    song_lists_matching[0]['artists'][0]['name'])
+                spotify.get_spotify().add_to_queue(uri=song_lists_matching[0]['uri'])
+                spotify.get_spotify().next_track()
+                # spotify.get_spotify().start_playback(context_uri=song_lists_matching[0]['uri'])
+
+    @intent_file_handler("pause_music.intent", "PauseSpotifyIntent")
+    def pause_music(self, data):
+        spotify.get_spotify().pause_playback()
+        print("[INFO INTENT] - Paused music for Spotify")
