88a232a7063d07cc36fccc2e4235e76d05f84358
Mathieu B <m4th1eu@protonmail.com>
2021-07-30 21:12:13 +0200

    using new method from language_utils and removed tests

--- jarvis/main.py
+++ jarvis/main.py
@@ -25,7 +25,7 @@ def process_request():
 if __name__ == '__main__':
     # Load lingua franca in the memory
     # Supported : English French German Hungarian Italian Portuguese Swedish
-    lingua_franca.load_language(lang=languages_utils.get_language().split("-")[0])
+    lingua_franca.load_language(lang=languages_utils.get_language_only_country())
 
     # Tests
     WikipediaSkill().register()
@@ -34,9 +34,7 @@ if __name__ == '__main__':
 
     intent_manager.load_all_skills()
 
-    intent_manager.recognise("cherche sur wikipédia Elon Musk")  # TO CHECK
-    intent_manager.recognise("c'est qui Elon Musk")  # TO CHECK
-    intent_manager.recognise("cherche Elon Musk sur wikipédia")  # TO CHECK
+    # intent_manager.recognise("cherche Elon Musk sur wikipédia")  # TO CHECK
     # intent_manager.recognise("raconte moi une blague")  # WORKING
     # intent_manager.recognise("joue le morceau crazy crazy nights de KISS sur spotify")  # WORKING
     # intent_manager.recognise("coupe la musique")  # WORKING
