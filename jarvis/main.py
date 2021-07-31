411ddec23784de1154b5a87d4b00ea3d38c1f510
Mathieu B <m4th1eu@protonmail.com>
2021-07-31 12:57:23 +0200

    Integration with the Client Speech to Text working :) (also moved flask related methods to flask_utils.py)

--- jarvis/main.py
+++ jarvis/main.py
@@ -1,26 +1,13 @@
-import flask
 import lingua_franca
-from flask import Flask, request, jsonify, Response
+from flask import Flask
 
 from jarvis.skills import intent_manager
 from jarvis.skills.entertainement.jokes import JokesSkill
 from jarvis.skills.entertainement.spotify import SpotifySkill
 from jarvis.skills.research.wikipedia import WikipediaSkill
-from jarvis.utils import languages_utils
-from utils import config_utils, flask_utils
+from jarvis.utils import languages_utils, flask_utils
 
 app = Flask(__name__)
-
-
-@app.route("/process", methods=['POST'])
-def process_request():
-    data = flask_utils.get_data_in_request(request)
-
-    if 'sentence' not in data or not data['sentence']:
-        flask.abort(Response('You must provide a \'sentence\' parameter (not empty aswell)!'))
-
-    return jsonify(intent_manager.recognise(sentence=data['sentence']))
-
 
 if __name__ == '__main__':
     # Load lingua franca in the memory
@@ -32,14 +19,15 @@ if __name__ == '__main__':
     JokesSkill().register()
     SpotifySkill().register()
 
+    # Load all skills
     intent_manager.load_all_skills()
 
+    # Bunch of tests
     # intent_manager.recognise("cherche Elon Musk sur wikipédia")  # WORKING
     # intent_manager.recognise("raconte moi une blague")  # WORKING
     # intent_manager.recognise("joue le morceau crazy crazy nights de KISS sur spotify")  # WORKING
     # intent_manager.recognise("coupe la musique")  # WORKING
     # intent_manager.recognise("c'est quoi le nom de cette chanson ?") # WORKING
 
-    # start the flask server
-    app.config['JSON_AS_ASCII'] = False
-    app.run(port=config_utils.get_in_config("PORT"), debug=False, host='0.0.0.0', threaded=True)
+    # Start the flask server
+    flask_utils.start_server()
--- jarvis/utils/flask_utils.py
+++ jarvis/utils/flask_utils.py
@@ -1,8 +1,40 @@
 import json
 
+import flask
+import speech_recognition as sr
+from flask import Response, request, jsonify
 
-def get_data_in_request(request):
-    data_str = str(request.data.decode('utf8')).replace('"', '\"').replace("\'", "'")
+from jarvis.main import app
+from jarvis.skills import intent_manager
+from jarvis.utils import config_utils, languages_utils
+
+
+@app.route("/process", methods=['POST'])
+def process_request():
+    data = get_data_in_request(request)
+
+    if 'sentence' not in data or not data['sentence']:
+        flask.abort(Response('You must provide a \'sentence\' parameter (not empty aswell)!'))
+
+    return jsonify(intent_manager.recognise(sentence=data['sentence']))
+
+
+@app.route("/process_audio_request", methods=['POST'])
+def process_audio_request():
+    frame_data = request.data
+    sample_rate = 44100
+    sample_width = 2
+
+    r = sr.Recognizer()
+    audio = sr.AudioData(frame_data, sample_rate, sample_width)
+
+    result_stt = r.recognize_google(audio, language=languages_utils.get_language_only_country())
+
+    return jsonify(intent_manager.recognise(sentence=result_stt))
+
+
+def get_data_in_request(flask_request):
+    data_str = str(flask_request.data.decode('utf8')).replace('"', '\"').replace("\'", "'")
 
     # if no data return an empty json to avoid error with json.loads below
     if not data_str:
@@ -14,3 +46,8 @@ def get_data_in_request(request):
         data_json = json.loads(data_json)
 
     return data_json
+
+
+def start_server():
+    app.config['JSON_AS_ASCII'] = False
+    app.run(port=config_utils.get_in_config("PORT"), debug=False, host='0.0.0.0', threaded=True)
