e598f69843139197290b163897efad52c23ea7d1
Mathieu B <m4th1eu@protonmail.com>
2021-08-01 19:01:58 +0200

    Improved speedtest skill

--- jarvis/skills/productivity/speedtest/__init__.py
+++ jarvis/skills/productivity/speedtest/__init__.py
@@ -11,21 +11,27 @@ class SpeedTestSkill(Skill, metaclass=SkillRegistering):
     @intent_file_handler("start_speedtest.intent", "StartSpeedTestIntent")
     def handle_speedtest(self, data):
         try:
-            self.speak_dialog('starting_speedtest')
+            self.speak_dialog_threaded('starting_speedtest')
+            result = start_speedtest()
 
-            speed = speedtest.Speedtest()
-            # speed.get_servers([])
-            speed.get_best_server()
-            speed.download()
-            speed.upload(pre_allocate=False)
-            # speed.results.share()
-            result = speed.results.dict()
-            down_speed = ('%.2f' % float((result["download"]) / 1000000))
-            up_speed = ('%.2f' % float((result["upload"]) / 1000000))
-
-            self.speak_dialog('result', {'DOWN': down_speed, 'UP': up_speed})
+            down_speed = result[0]
+            up_speed = result[1]
+            self.speak_dialog('result_speedtest', {'DOWN': down_speed, 'UP': up_speed})
         except:
-            self.speak_dialog("error")
+            self.speak("Error speedtest")
+
+
+def start_speedtest():
+    speed = speedtest.Speedtest(timeout=2)
+    speed.get_servers([])
+    speed.get_best_server()
+    speed.download()
+    speed.upload(pre_allocate=False)
+    result = speed.results.dict()
+    down_speed = ('%.2f' % float((result["download"]) / 1000000))
+    up_speed = ('%.2f' % float((result["upload"]) / 1000000))
+
+    return [down_speed, up_speed]
 
 
 def create_skill(data):
