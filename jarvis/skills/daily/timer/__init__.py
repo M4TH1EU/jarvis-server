214e8a6a9c1be9150d7821e3cc6180ad23679bef
Mathieu B <m4th1eu@protonmail.com>
2021-08-02 18:23:11 +0200

    Improved timer skill intent

--- jarvis/skills/daily/timer/__init__.py
+++ jarvis/skills/daily/timer/__init__.py
@@ -1,5 +1,8 @@
+from lingua_franca.parse import extract_duration
+
 from jarvis.skills import Skill, SkillRegistering
 from jarvis.skills.decorators import intent_file_handler
+from jarvis.utils import languages_utils
 
 
 class TimerSkill(Skill, metaclass=SkillRegistering):
@@ -9,6 +12,18 @@ class TimerSkill(Skill, metaclass=SkillRegistering):
     @intent_file_handler("start_timer.intent", "StartTimerIntent")
     def handle_start_timer(self, data):
         print(data)
+        if 'amount' in data and 'time_unit' in data:
+            print(extract_duration(data['amount'] + " " + data['time_unit']), languages_utils.get_language())
+
+            if 'name' in data:
+                print("Start timer for {} {} named {}".format(data['amount'], data['time_unit'], data['name']))
+                pass
+            else:
+                print("Start timer for {} {} without name".format(data['amount'], data['time_unit']))
+                # TODO : ask for name
+                pass
+        else:
+            print("No amount and/or time_unit")
         pass
 
 
